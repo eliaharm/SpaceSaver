81f2ff74868969fc9a28c9c4aa7d0bfa9d26569d
elia <elia@DESKTOP-7F4GCNK.local>
2021-12-12 19:30:13 +0100

    updated path and new image magik command for photos

--- convert_Pix_Multi.bat
+++ convert_Pix_Multi.bat
@@ -4,8 +4,8 @@ echo ping.exe  > commands.txt
 for /F "tokens=*" %%A in (Photos.txt) do (
 echo %%~nA %%~xA
 for %%B in ("%%A\..") do (
-mkdir "D:\Photos\tmp_photos\%%~nxB" 2>nul
- echo C:\PortableApps\ImageMagick-7.0.8-10-portable-Q16-x64\convert.exe "%%~A"  -colorspace RGB -filter Lanczos  -define filter:blur=0.88451002338585141  -define filter:lobes=2 -resize "2200x2200>" -colorspace sRGB -normalize -sigmoidal-contrast 1.5 -colorspace HCL -channel g -sigmoidal-contrast 1.0,0%% +channel -colorspace sRGB +repage  -quality 92 "D:\Photos\tmp_photos\%%~nxB\%%~nA.jpg"  >> commands.txt
+mkdir "C:\Photos\tmp_photos\%%~nxB" 2>nul
+ echo magick convert "%%~A"  -colorspace RGB -filter Lanczos  -define filter:blur=0.88451002338585141  -define filter:lobes=2 -resize "2200x2200>" -colorspace sRGB -normalize -sigmoidal-contrast 1.5 -colorspace HCL -channel g -sigmoidal-contrast 1.0,0%% +channel -colorspace sRGB +repage  -quality 92 "C:\Photos\tmp_photos\%%~nxB\%%~nA.jpg"  >> commands.txt
 
 )
 )
